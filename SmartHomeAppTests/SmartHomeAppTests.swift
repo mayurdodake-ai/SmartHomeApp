f8d7a48535b361b583d1d3f018091a88bfbfa1a2
Mayur Dodake <mayur.dodake@softdel.com>
2025-09-26 14:15:13 +0530

    Initial Commit

---
+++ SmartHomeAppTests/SmartHomeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeAppTests.swift
+//  SmartHomeAppTests
+//
+//  Created by Mayur Dodake on 26/09/25.
+//
+
+import Testing
+@testable import SmartHomeApp
+
+struct SmartHomeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
